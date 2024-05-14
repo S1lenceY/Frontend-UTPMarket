19e3f79ece6ba6a61d241495aaf9e3ffa49c2040
Alejo <davimitor04@gmail.com>
2024-05-14 13:30:15 -0500

    Añadiendo función de ejemplo(luego se borra)

--- src/Path/Apis.jsx/getProductos.jsx
+++ src/Path/Apis.jsx/getProductos.jsx
@@ -1,5 +1,5 @@
 export default async function getProducts() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/productos');
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const json = await response.json();
     return json;
   }
