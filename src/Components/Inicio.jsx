ee19a36c82442fd5c8f23c8b05a9069ade0985b5
Alejo <davimitor04@gmail.com>
2024-05-14 14:56:08 -0500

    Actualización de Inicio y Productos

--- src/Components/Inicio.jsx
+++ src/Components/Inicio.jsx
@@ -8,8 +8,8 @@ const Inicio = () => {
     <>
       <div className="flex flex-col">
         <SwipeCarousel />
-        <div className="bg-white w-fit p-2 text-lg md:p-3 lg:pr-80 md:text-2xl text-[#3E4558] gap-2 flex">
-          <span>|</span> <b>Regalos</b> que harán <b>sonreír</b> a <b className="text-[#FF395C]">Mamá</b>
+        <div className="bg-white w-fit p-2 text-lg md:p-3 lg:pr-80 md:text-2xl text-[#3E4558] gap-1 md:gap-2 flex">
+          <span>|</span><b>Regalos</b>que harán<b>sonreír</b>a<b className="text-[#FF395C]">Mamá</b>
         </div>
         <BestProducts />
       </div>
