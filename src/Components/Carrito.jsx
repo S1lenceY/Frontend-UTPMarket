0871b62376ad0550204ace978a8eaaca8cf75f35
Alejo <davimitor04@gmail.com>
2024-05-14 02:05:55 -0500

    Carrito con modal incluido

--- src/Components/Carrito.jsx
+++ src/Components/Carrito.jsx
@@ -1,7 +1,32 @@
-import React from "react";
-import { AiOutlineDollar } from "react-icons/ai";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Carrito = () => {
+  //Para abrir modal
+  const [showModal, setShowModal] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  //Definiendo los métodos de pago
+  const pago = [
+    {
+      name: "VISA",
+      img: "/src/Assets/VISA.jpg",
+      category: "Banca Movil",
+    },
+    {
+      name: "VISA",
+      img: "/src/Assets/VISA.jpg",
+      category: "Banca Movil",
+    },
+  ];
+
   //Cambiar esto por la recepción de datos que me den en el API
   const data = [
     {
@@ -38,7 +63,7 @@ const Carrito = () => {
       category: "Panaderia",
       price: 15,
       count: 2,
-    }
+    },
   ];
 
   return (
@@ -49,10 +74,9 @@ const Carrito = () => {
         </div>
         <div className="flex">
           <div className="flex gap-10 mt-4">
-            <div className=" flex-col gap-5 max-h-[600px] overflow-y-auto scroll hidden md:flex">
+            <div className=" flex-col gap-5 max-h-[424px] overflow-y-auto scroll hidden md:flex">
               {data.map((d) => (
                 <div className="bg-white text-black rounded-md w-fit flex mr-3">
-
                   <div className="flex">
                     <img src={d.img} className="rounded-s-md w-64 h-32 " />
                     <div className=" bg-yellow-200 h-full w-2"></div>
@@ -72,39 +96,139 @@ const Carrito = () => {
                       </span>
                     </div>
                   </div>
-                  
                 </div>
               ))}
             </div>
             <div className="flex flex-col">
               <div className="bg-white px-10 py-7 ">
                 <div className="border-b border-b-black">
-                  <span className="text-xl font-bold ">
-                    Datos Generales
+                  <span className="text-xl font-bold ">Datos Generales</span>
+                </div>
+                <div className="flex flex-col text-sm py-3 border-b border-b-black">
+                  <span>
+                    <b>Subtotal: </b>S/100.00
+                  </span>
+                  <span>
+                    <b>IGV 18%: </b>S/18.00
                   </span>
                 </div>
                 <div className="flex flex-col text-sm py-3 border-b border-b-black">
-                  <span><b>Subtotal: </b>S/100.00</span>
-                  <span><b>IGV 18%: </b>S/18.00</span>
+                  <span>
+                    <b>Coins Ganadas: </b>12
+                  </span>
+                  <span>
+                    <b>Lugar de Recepción: </b>UTP Sede Chiclayo
+                  </span>
                 </div>
                 <div className="flex flex-col text-sm py-3 border-b border-b-black">
-                  <span><b>Coins Ganadas: </b>12</span>
-                  <span><b>Lugar de Recepción: </b>UTP Sede Chiclayo</span>
-                </div>
-                <div className="flex flex-col text-sm py-3 border-b border-b-black">
-                  <span><b>Total a pagar: </b>S/112.00</span>
+                  <span>
+                    <b>Total a pagar: </b>S/112.00
+                  </span>
                 </div>
               </div>
 
               <div className="flex mt-3">
-                <input type="text" placeholder="UTP-CIX-24" className="py-2 md:p-2 px-3 outline-none border"/>
-                <div className="flex items-center px-2 md:p-2 md:px-4 bg-slate-300 text-slate-500 font-bold border">Canjear cupón</div>
+                <input
+                  type="text"
+                  placeholder="UTP-CIX-24"
+                  className="py-2 md:p-2 px-3 outline-none border"
+                />
+                <div className="flex items-center px-2 md:p-2 md:px-4 bg-slate-300 text-slate-500 font-bold border">
+                  Canjear cupón
+                </div>
               </div>
-              <button className="w-full bg-[#000f37] text-white mt-3 p-2 rounded font-bold text-sm">PROCESAR COMPRA</button>
+              <button
+                className="w-full bg-[#000f37] text-white mt-3 p-2 rounded font-bold text-sm"
+                onClick={handleButtonClick}
+              >
+                PROCESAR COMPRA
+              </button>
             </div>
           </div>
         </div>
       </div>
+
+      <AnimatePresence>
+        {showModal && (
+          <motion.div
+            className="fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm flex justify-center items-center overflow-y-auto"
+            onClick={handleCloseModal}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <motion.div
+              className="flex w-fit h-fit bg-transparent text-white"
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.8 }}
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <div className="w-fit flex transform overflow-hidden rounded bg-background p-5 text-left align-middle shadow-xl transition-all text-black">
+                <div className="hidden lg:block">
+                  <img src="/src/Assets/FondoLoader.webp" className=" w-72" />
+                </div>
+                <div className="flex flex-col ml-4">
+                  <div className="bg-white p-3 text-lg md:p-3 md:text-2xl gap-2 flex font-bold">
+                    <span>|</span> Métodos de Pago
+                  </div>
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+                    {pago.map((p) => (
+                      <div className="bg-white text-black rounded-md w-fit flex flex-col m-3">
+                        <div className="flex flex-col">
+                          <img
+                            src={p.img}
+                            className="rounded-t-md w-full h-32 "
+                          />
+                          <div className=" bg-yellow-200 w-full h-2"></div>
+                        </div>
+
+                        <div className="flex mt-2 justify-between p-3">
+                          <div className="flex flex-col">
+                            <span className="font-bold">{p.name}</span>
+                            <span className="text-sm">{p.category}</span>
+                          </div>
+                          <div className="self-end">
+                            <button className=" p-1 px-3 rounded text-white text-sm bg-[#000f37] ">
+                              Elegir
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+
+                    <div className="bg-white text-black rounded-md w-fit flex flex-col m-3">
+                      <div className="flex flex-col">
+                        <img
+                          src="/src/Assets/Logo.webp"
+                          className="rounded-t-md w-56 h-32 "
+                        />
+                        <div className=" bg-yellow-200 w-full h-2"></div>
+                      </div>
+
+                      <div className="flex mt-2 justify-between p-3">
+                        <div className="flex flex-col">
+                          <span className="font-bold">CODE</span>
+                          <span className="text-sm">Pago en Caja</span>
+                        </div>
+                        <div className="self-end">
+                          <button className=" p-1 px-3 rounded text-white text-sm bg-[#000f37] ">
+                            Elegir
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
--- src/Components/Inicio.jsx
+++ src/Components/Inicio.jsx
@@ -6,7 +6,7 @@ import BestProducts from "./Utils/BestProducts";
 const Inicio = () => {
   return (
     <>
-      <div className="flex flex-col overflow-hidden">
+      <div className="flex flex-col">
         <SwipeCarousel />
         <div className="bg-white w-fit p-2 text-lg md:p-3 lg:pr-80 md:text-2xl gap-2 flex">
           <span>|</span> Regalos que harán sonreír a Mamá
